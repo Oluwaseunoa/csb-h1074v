0a580255be2ed64eab5d4fe824f93a64919376d4
Code Sandbox <hello@codesandbox.io>
2022-11-07 10:03:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.79c834e82e75abca3e6b0ff4185b9bb6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941557af68d0187b9710cd8f563fae17",
+    "url": "https://oluwaseunoa.github.io/csb-h1074v/index.html"
+  },
+  {
+    "revision": "4fd7a81c038202ac1436",
+    "url": "https://oluwaseunoa.github.io/csb-h1074v/static/js/2.824d600c.chunk.js"
+  },
+  {
+    "revision": "0468d4fad5f132c609d4",
+    "url": "https://oluwaseunoa.github.io/csb-h1074v/static/js/main.2f1a1aca.chunk.js"
+  },
+  {
+    "revision": "df008aa94e42b85ee8b3",
+    "url": "https://oluwaseunoa.github.io/csb-h1074v/static/js/runtime-main.d271df6f.js"
+  }
+]);
